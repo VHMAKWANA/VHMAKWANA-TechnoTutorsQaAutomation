9835f71ce0d9c30ed096606e161b8afd35139bb2
VHMAKWANA <vijaymakwana070@gmail.com>
2020-03-26 19:30:44 -0400

    a

--- src/main/java/Com/Techno/Qa/Pages/HomePage.java
+++ src/main/java/Com/Techno/Qa/Pages/HomePage.java
@@ -183,7 +183,7 @@ public class HomePage extends TechnoBase {
         System.out.println(Errorsend.getText());
 	}
 	
- 
+//contact us
 	public void booktutor(String name, String email, String subject, String message) {
 
 		Name1.sendKeys(name);
